d6bbc3ca3ccbb328270e92419273ce39f660bc88
Jakub Brych <96384633+brychjakub@users.noreply.github.com>
2022-11-28 17:21:39 +0100

    Update another_take_on_shooter.py

--- another_take_on_shooter.py
+++ another_take_on_shooter.py
@@ -8,15 +8,15 @@ pygame.init()
 WINDOW_WIDTH = 945
 WINDOW_HEIGHT = 600
 
-OBJECT_WIDTH = 50
-OBJECT_HEIGHT = 50
+OBJECT_WIDTH = 80
+OBJECT_HEIGHT = 80
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("fly and shoot")
 
 velocity = 5
 angle = 0
 
-BACKGROUND_IMAGE = pygame.transform.scale(pygame.image.load("space.png"), (WINDOW_WIDTH, WINDOW_HEIGHT))
+BACKGROUND_IMAGE = pygame.image.load("space.png")
 BACKGROUND_IMAGE_RECT = BACKGROUND_IMAGE.get_rect()
 
 
@@ -24,18 +24,17 @@ FPS = 60
 clock = pygame.time.Clock()
 
 class Player1(pygame.sprite.Sprite):
-    def __init__(self,x, y, velocity):
+    def __init__(self,x, y, velocity, angle):
         super().__init__()
         self.image = pygame.transform.scale(pygame.image.load("spaceship_red.png"), (OBJECT_WIDTH, OBJECT_HEIGHT))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.velocity = velocity
-        self.angle = 0
+        self.angle = angle
     
     def update(self):
          self.move()
          self.rotate()
-         self.shooting()
 
     def move(self):       
         keys = pygame.key.get_pressed()
@@ -49,31 +48,18 @@ class Player1(pygame.sprite.Sprite):
             self.rect.y += self.velocity
         
     def rotate(self):
+        self.angle = 0
         keys = pygame.key.get_pressed()
 
         if keys[K_w]:
-            self.angle += 6            
-            mx, my = 250,250
-            img_copy = pygame.transform.rotate(self.image, self.angle)
-            display_surface.blit(img_copy, (mx - int(img_copy.get_width() / 2), my - int(img_copy.get_height() / 2)))
-
-    def shooting(self):
-        pass
-
-
-class Planet(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load("planet.png"), (OBJECT_WIDTH, OBJECT_HEIGHT))
-        self.rect = self.image.get_rect()
-        self.rect = (random.randint(0, WINDOW_WIDTH-80),random.randint(0, WINDOW_HEIGHT-80))
-
-    def update(self):
-        pass
+                self.angle += 6            
+                mx, my = self.rect.centerx, self.rect.centery
+                img_copy = pygame.transform.rotate(self.image, angle)
+                display_surface.blit(img_copy, (mx - int(img_copy.get_width() / 2), my - int(img_copy.get_height() / 2)))
+                
 
 
 
-    
 class Explosion(pygame.sprite.Sprite):
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
@@ -104,31 +90,24 @@ class Explosion(pygame.sprite.Sprite):
 			self.kill()
 
 player_group = pygame.sprite.Group() 
-red_player = Player1(0, 0, velocity)
+red_player = Player1(0, 0, velocity, angle)
 player_group.add(red_player)
-
-
-planet_group = pygame.sprite.Group()
-planet1 = Planet()
-planet_group.add(planet1)
-
 
 running = True
 while running:
+    angle += 6
     #Check to see if the user wants to quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-   
-
+           
     #Blit the background
     display_surface.blit(BACKGROUND_IMAGE, BACKGROUND_IMAGE_RECT)
     #Update display and tick clock
+
     player_group.update()
     player_group.draw(display_surface)
-    planet_group.update()
-    planet_group.draw(display_surface)
     pygame.display.update()
     clock.tick(FPS)
 
